0aeb2fc80b3841f73e2078878bca7424012a1385
Charles Jordan <charlesjordan@Charless-iMac.local>
2022-04-21 17:42:39 -0500

    added testing docs and comments

--- src/main/java/easy/AdjacentElementsProduct.java
+++ src/main/java/easy/AdjacentElementsProduct.java
@@ -1,7 +1,16 @@
 package easy;
 
+/**
+ * Given an array of integers, find the pair of adjacent elements that has the largest product
+ * and return that product.
+ */
 public class AdjacentElementsProduct {
 
+    /**
+     * Method that finds the largest multiplicative sum of two adjacent integers in an array
+     * @param array of integers
+     * @return integer sum
+     */
     public int findLargestProduct(int[] array) {
 
         int result = Integer.MIN_VALUE;
