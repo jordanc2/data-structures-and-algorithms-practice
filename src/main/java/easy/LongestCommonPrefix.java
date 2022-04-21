887e62da76cd052a5ba0e2fabe0fa3c53f836d45
Charles Jordan <charlesjordan@Charless-iMac.local>
2022-04-21 16:45:53 -0500

    added comments and docs

--- src/main/java/easy/LongestCommonPrefix.java
+++ src/main/java/easy/LongestCommonPrefix.java
@@ -12,14 +12,20 @@ public class LongestCommonPrefix {
      */
     public String findLongestCommonPrefix(String[] array) {
 
-        if (array.length == 0) {
+        if (array.length == 0) { /* Null array check, if true, return an empty string */
             return "";
         }
 
+        /* Instantiates the String result and initializes it to the first element of the array */
         String result = array[0];
 
-        for (int i = 0; i < array.length; i++) {
-            while (!array[i].startsWith(result)) {
+        /*
+        * Imperative for loop to iterate through the 'array' parameter
+        *       while the current index does NOT begin with the element stored in result
+        *           result is set to a substring ranging from the first char to last checked char
+        */
+        for (String s : array) {
+            while (!s.startsWith(result)) {
                 result = result.substring(0, result.length() - 1);
             }
         }
