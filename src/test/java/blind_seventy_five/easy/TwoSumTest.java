998afbf3339091ad85aea179e3933e3e83aac627
Charles Jordan <charlesjordan@Charless-iMac.local>
2022-04-25 21:12:41 -0500

    added test for twosum

--- src/test/java/blind_seventy_five/easy/TwoSumTest.java
+++ src/test/java/blind_seventy_five/easy/TwoSumTest.java
@@ -6,7 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TwoSumTest {
 
+    private int[] testArray;
+
     @Test
-    void findTwoSum() {
+    void findTwoSum_givenValidArrayOfIntegers_returnsTwoIndexesWithIntegersThatAddUpToTarget() {
+        // GIVEN
+        testArray = new int[]{2,7,11,15};
+        int target = 9;
+
+        // WHEN
+        TwoSum twoSum = new TwoSum();
+        int[] result = twoSum.findTwoSum(testArray, target);
+
+        // THEN
+        int[] expected = new int[]{0,1};
+        assertArrayEquals(expected, result);
     }
 }
